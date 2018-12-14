110557225fd6725600bc62aa1933fb8b8432579a
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-14 21:02:40 +0700

    Update

--- bin/image.php
+++ bin/image.php
@@ -1,4 +1,16 @@
 <?php
 
-$a = google_image("es teh segar", 3);
+$pch = "";
+$pauth = "";
+$useProxy = 0;
+if (file_exists($f = __DIR__."/../proxy.json")) {
+	$f = json_decode(file_get_contents($f), true);
+	if (isset($f["ch"], $f["username"], $f["password"])) {
+		$pch = $f["ch"];
+		$useProxy = 1;
+		$pauth = "{$f["username"]}:{$f["password"]}";
+	}
+}
+
+$a = google_image("es teh segar", 3, $useProxy, $pch, $pauth);
 var_dump($a);
